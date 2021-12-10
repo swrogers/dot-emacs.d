9ed804dc85b1cc3471ae5b8f6420d4faa1cdd6ae
Shane Rogers <swrogers@gmail.com>
2021-12-10 09:33:56 -0600

    Ignore native comp warnings in early-init.el

--- early-init.el
+++ early-init.el
@@ -1,1 +1,9 @@
+;; straight.el does not use anything related
+;; to package.el at all
 (setq package-enable-at-startup nil)
+
+;; native compilation loves to pop warnings
+;; and does so INCESSANTLY
+(setq comp-async-report-warnings-errors nil)
+(setq warning-minimum-level :error)
+
